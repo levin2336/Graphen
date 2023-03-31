71c49e5b14386ce8093e049850d733c0df78da37
levin2336 <levin-janssen@web.de>
2023-03-31 12:26:36 +0200

    add Vericie

--- src/Graph.java
+++ src/Graph.java
@@ -1,12 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Graph {
 
     boolean[][] matrix;
+    List<Character> charList;
+
     public Graph(int anzahl) {
         matrix = new boolean[anzahl][anzahl];
+        charList = new ArrayList<>();
     }
 
     public void addVertice(char k){
-
+        if (charList.size() >= matrix.length){
+            return;
+        }
+        charList.add(k);
     }
 
     public int searchVericeIndex(char zeichen){
