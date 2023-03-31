7ebad83f18b9fdfa4953a4fd65a1ee9d0f2bffdf
levin2336 <levin-janssen@web.de>
2023-03-31 12:15:33 +0200

    drt

---
+++ src/Graph.java
@@ -0,0 +1,24 @@
+public class Graph {
+
+    int[][]
+
+    public Graph() {
+
+    }
+
+    public void addVertice(char k){
+
+    }
+
+    public int searchVericeIndex(char zeichen){
+        return 0;
+    }
+
+    public void addEdge(char k1, char k2){
+
+    }
+
+    public boolean hasEdge(char k1, char k2){
+        return true;
+    }
+}
