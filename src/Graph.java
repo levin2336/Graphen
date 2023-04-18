8498a4ff011275cb90ecad967d12fdd4b86fce1f
henning.ruwwe <henning4454@gmail.com>
2023-04-18 12:05:28 +0200

    isCircular

--- src/Graph.java
+++ src/Graph.java
@@ -12,6 +12,21 @@ public class Graph {
         charList = new ArrayList<>();
     }
 
+    public boolean equals (Graph input) {
+        boolean[][] m1 = this.matrix;
+        boolean[][] m2 = input.getMatrix();
+        if (m1.length != m2.length) return false;
+        for (int i = 0; i < m1.length; i++) {
+            if (m1[i].length != m2[i].length) return false;
+            for (int j = 0; j < m1[i].length; j++) {
+                boolean b1 = m1[i][j];
+                boolean b2 = m2[i][j];
+                if (b1 != b2) return false;
+            }
+        }
+        return true;
+    }
+
     public void addVertice(char k){
         if (charList.size() >= matrix.length){
             throw new ArrayIndexOutOfBoundsException("all Vertices are already set");
@@ -57,9 +72,10 @@ public class Graph {
             }
             usedEdges.addEdge(s.charAt(i), s.charAt(i+1));
         }
-        return (hasEdge(s.charAt(0), s.charAt(s.length()-1)));
-
+        return equals(usedEdges);
     }
+
+
 
     public boolean[][] getMatrix() {
         return matrix;
